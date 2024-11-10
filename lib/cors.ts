2848cd74bc16c5e2304c89c1c36bf68619eacb62
mdsibtainhabib <sibtainhabib413@gmail.com>
2024-11-10 16:30:21 +0530

    some commit

--- lib/cors.ts
+++ lib/cors.ts
@@ -3,7 +3,7 @@ import Cors from 'cors'
 // Initialize CORS
 const cors = Cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  origin: 'https://life.vercel.app', // Replace with your frontend domain
+  origin: process.env.NEXT_PUBLIC_BASE_URL, // Replace with your frontend domain
   credentials: true, // Allow cookies to be sent with requests
 });
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
