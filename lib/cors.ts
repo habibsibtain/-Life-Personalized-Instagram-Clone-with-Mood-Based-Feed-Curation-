7217d2a40963503545aecce9d6d4f122cdfb4a0d
mdsibtainhabib <sibtainhabib413@gmail.com>
2024-11-10 16:24:34 +0530

    added cors mixing value

--- lib/cors.ts
+++ lib/cors.ts
@@ -3,7 +3,7 @@ import Cors from 'cors'
 // Initialize CORS
 const cors = Cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  origin: 'https://life-personalized-instagram-clone-with-mood-based-o1mu1vstk.vercel.app', // Replace with your frontend domain
+  origin: 'https://life.vercel.app', // Replace with your frontend domain
   credentials: true, // Allow cookies to be sent with requests
 });
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
