e98384c8933df3fadc9aa9a7e051b141b744bae0
mdsibtainhabib <sibtainhabib413@gmail.com>
2024-11-10 14:55:48 +0530

    add Pathname

--- app/api/posts/comment/[postId]/route.ts
+++ app/api/posts/comment/[postId]/route.ts
@@ -1,12 +1,17 @@
 import dbConnect from "@/lib/dbConnect";
 import Post from "@/models/postSchema";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { postId: string } }) {
+export async function GET(req: NextRequest) {
   await dbConnect();
-  
+
   try {
-    const { postId } = params;
+    // Accessing the route parameters from the request URL
+    const postId = req.nextUrl.pathname.split("/").pop(); // Gets the postId from the URL
+    
+    if (!postId) {
+      return NextResponse.json({ success: false, message: "Post ID not provided" }, { status: 400 });
+    }
 
     const post = await Post.findById(postId)
       .populate({
